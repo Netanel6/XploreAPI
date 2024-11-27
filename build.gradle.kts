02b9122eed29896f694960c0040908d64beecf61
Netanel Amar <NetanelCA2@gmail.com>
2024-11-27 19:34:11 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "org.netanel"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val exposedVersion = "0.56.0"
+    val ktorVersion = "3.0.0"
+    testImplementation(kotlin("test"))
+    // Logging dependency is required for server-side applications; omitting it may cause compilation issues
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+
+    // Ktor
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-resources:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+
+    // Exposed DB
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+    implementation("com.h2database:h2:1.4.200")
+
+
+    implementation("com.vladsch.flexmark:flexmark-all:0.64.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(18)
+}
