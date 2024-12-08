77608c5afc0865bc651a68a95d2b0b558c6d8783
Netanel Amar <Namar@Invendiv.com>
2024-12-08 12:04:01 +0200

    Update EnvironmentConfig.kt

--- src/main/kotlin/util/EnvironmentConfig.kt
+++ src/main/kotlin/util/EnvironmentConfig.kt
@@ -9,7 +9,7 @@ object EnvironmentConfig {
         .load()
 
     val enableSsl: String by lazy { dotenv["ENABLE_SSL"] ?: "false" }
-    val mongoDbUri: String by lazy { dotenv["MONGODB_URI"] ?: error("MONGODB_URI is not defined") }
+    val mongoDbUri: String by lazy { dotenv["MONGODB_URI"] ?: "mongodb+srv://netanelca2:oS29Q3LiSBtazPS4@xplorecluster.jkgx7.mongodb.net" }
     val env: String by lazy { dotenv["ENVIRONMENT"] ?: error("ENVIRONMENT is not defined") }
     val apiUrl: String by lazy { dotenv["API_URL"] ?: error("API_URL is not defined") }
 }
