cd683cea69fbed6c9c936ca5f3a7ea654da6b5d7
Netanel Amar <NetanelCA2@gmail.com>
2025-02-28 17:53:07 +0200

    Update Quiz.kt

--- src/main/kotlin/quiz/repository/model/Quiz.kt
+++ src/main/kotlin/quiz/repository/model/Quiz.kt
@@ -10,14 +10,9 @@ data class Quiz(
     @Serializable(with = ObjectIdSerializer::class)
     val _id: ObjectId? = null,
     val questions: List<QuestionV2>,
-    val timer: Int,
+    val quizTimer: Int,
+    val answerLockTimer: Int,
     val title: String? = null,
-    val description: String? = null,
-    val difficulty: DifficultyLevel? = null,
     val creatorId: String? = null,
-    val startTime: String? = null,
-    val endTime: String? = null,
-    val maxParticipants: Int? = null,
-    val tags: List<String>? = null,
     val isActive: Boolean = true
 )
